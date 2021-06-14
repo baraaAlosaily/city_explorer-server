604a16dde795661f8712a78d722328ba2c1ace2b
baraaAlosaily <seelbaraa@gmail.com>
2021-06-15 00:10:25 +0300

    try to deploy

--- server.js
+++ server.js
@@ -1,16 +1,16 @@
-const express = require("express"); // require the express package
+const express = require('express'); // require the express package
 const app = express(); // initialize your express app instance
 
-const cors = require("cors");
+const cors = require('cors');
 
 app.use(cors()); // after you initialize your express app instance
 // a server endpoint
-require("dotenv").config();
+require('dotenv').config();
 
-const port = process.env.port;
-app.get("/", function (req, res) {
+const port = process.env.PORT || 3000;
+app.get('/', function (req, res) {
   // callback function of what we should do with our request
-  res.send("Hello World"); // our endpoint function response
+  res.send('Hello World'); // our endpoint function response
 });
 
 app.listen(port, () => {
