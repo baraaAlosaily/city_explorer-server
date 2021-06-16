387d4563ac5eb4e232dd84d6ac981e31fcc170df
baraaAlosaily <seelbaraa@gmail.com>
2021-06-17 02:33:31 +0300

    lab07+lab08

--- server.js
+++ server.js
@@ -12,6 +12,7 @@ require('dotenv').config();
 
 const PORT = process.env.PORT || 4000;
 const WEATHER_BIT_KEY = process.env.WEATHER_BIT_KEY;
+const MOVIE_KEY = process.env.MOVIE_KEY;
 
 app.get('/test', (req, res) => {
   let str = 'hello from back end ';
@@ -37,10 +38,57 @@ app.get('/weather', (req, res) => {
   }
 });
 
+app.get('/movies', (req, res) => {
+  let cityName = req.query.query;
+  axios
+    .get(
+      `https://api.themoviedb.org/3/search/movie?api_key=${MOVIE_KEY}&query=${cityName}`
+    )
+    .then((response) => {
+      const arrOfMovies = [];
+      response.data.results.map((item) => {
+        let imageURL = `https://image.tmdb.org/t/p/w500${item.poster_path}`;
+        let movieObject = new Movie(
+          item.title,
+          item.overview,
+          item.vote_average,
+          item.vote_count,
+          imageURL,
+          item.popularity,
+          item.release_date
+        );
+        arrOfMovies.push(movieObject);
+      });
+      res.send(arrOfMovies);
+    })
+    .catch((error) => {
+      res.send(error.message);
+    });
+});
+
 class Weather {
   constructor(weatherdata) {
     this.description = weatherdata.weather.description;
     this.date = weatherdata.valid_date;
+  }
+}
+class Movie {
+  constructor(
+    title,
+    overview,
+    averageVotes,
+    totalVotes,
+    imgUrl,
+    popularity,
+    releasedOn
+  ) {
+    this.title = title;
+    this.overview = overview;
+    this.average_votes = average_votes;
+    this.total_votes = total_votes;
+    this.image_url = image_url;
+    this.popularity = popularity;
+    this.released_on = released_on;
   }
 }
 
