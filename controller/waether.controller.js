6670f79c24fa21901732a531ae4c06711df61676
baraaAlosaily <seelbaraa@gmail.com>
2021-06-18 23:58:09 +0300

    lab10

--- controller/waether.controller.js
+++ controller/waether.controller.js
@@ -2,21 +2,34 @@ const axios = require('axios');
 const Weather = require('../model/weather.model');
 require('dotenv').config();
 const WEATHER_BIT_KEY = process.env.WEATHER_BIT_KEY;
+const Cache = require('../helper/Cache');
+const cacheObj = new Cache();
 const weatherController = (req, res) => {
   const lat = req.query.lat;
   const lon = req.query.lon;
+  const weatherKey = `weather${lon}-${lat}`;
   if (lat && lon) {
-    const weatherBitUrl = `https://api.weatherbit.io/v2.0/forecast/daily?key=${WEATHER_BIT_KEY}&lat=${lat}&lon=${lon}`;
-    console.log(weatherBitUrl);
-    axios
-      .get(weatherBitUrl)
-      .then((response) => {
-        const resposeData = response.data.data.map((obj) => new Weather(obj));
-        res.status(200).json(resposeData);
-      })
-      .catch((error) => {
-        res.status(500).send(error.message);
-      });
+    if (
+      cacheObj[weatherKey] &&
+      Date.now() - cacheObj[weatherKey].timestamp < 86400000
+    ) {
+      res.json(cacheObj[weatherKey]);
+    } else {
+      const weatherBitUrl = `https://api.weatherbit.io/v2.0/forecast/daily?key=${WEATHER_BIT_KEY}&lat=${lat}&lon=${lon}`;
+      console.log(weatherBitUrl);
+      axios
+        .get(weatherBitUrl)
+        .then((response) => {
+          const resposeData = response.data.data.map((obj) => new Weather(obj));
+          cacheObj[weatherKey] = resposeData;
+          cacheObj[weatherKey].timestamp = Date.now();
+          console.log(cacheObj[weatherKey]);
+          res.status(200).json(resposeData);
+        })
+        .catch((error) => {
+          res.status(500).send(error.message);
+        });
+    }
   } else {
     res.send('Please Enter Proper Lat &Lon ');
   }
